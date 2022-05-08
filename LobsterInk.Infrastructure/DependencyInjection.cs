44a8eadfe0e0aff3ba0497fa9a774422de5f0571
Hannan <hannansultan@outlook.com>
2022-05-08 19:27:08 +0500

    Added DI for infrastructure project.

---
+++ LobsterInk.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,31 @@
+﻿using LobsterInk.Application.Interfaces;
+using LobsterInk.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LobsterInk.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            if (configuration.GetValue<bool>("UseInMemoryDatabase"))
+            {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseInMemoryDatabase("AdventureDb"));
+            }
+            else
+            {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(
+                        configuration.GetConnectionString("DefaultConnection")));
+            }
+
+            services.AddScoped<IApplicationDbContext>(provider => provider.GetRequiredService<ApplicationDbContext>());
+
+            return services;
+        }
+    }
+}
