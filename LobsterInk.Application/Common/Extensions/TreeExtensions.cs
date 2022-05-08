b8300b24cbcddfe7c1efed2fcc80a03e57821884
Hannan <hannansultan@outlook.com>
2022-05-09 00:37:54 +0500

    Iniital Create Tree extensions.

---
+++ LobsterInk.Application/Common/Extensions/TreeExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace LobsterInk.Application.Common.Extensions
+{
+    public static class TreeExtensions
+    {
+        public static IEnumerable<TreeItem<T>> GenerateTree<T, K>(
+            this IEnumerable<T> collection,
+            Func<T, K> idSelector,
+            Func<T, K> parentIdSelector,
+            K rootId = default(K))
+        {
+            foreach (var c in collection.Where(c => EqualityComparer<K>.Default.Equals(parentIdSelector(c), rootId)))
+            {
+                yield return new TreeItem<T>
+                {
+                    Item = c,
+                    Children = collection.GenerateTree(idSelector, parentIdSelector, idSelector(c))
+                };
+            }
+        }
+    }
+
+    public class TreeItem<T>
+    {
+        public T Item { get; set; }
+        public IEnumerable<TreeItem<T>> Children { get; set; }
+    }
+}
