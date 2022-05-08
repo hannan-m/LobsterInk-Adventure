095982ca1f99a5e656888fa3756dbad4b261998a
Hannan <hannansultan@outlook.com>
2022-05-08 19:20:43 +0500

    Initial Create ApplicationDbContext

---
+++ LobsterInk.Application/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using LobsterInk.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LobsterInk.Application.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Adventure> Adventures { get; }
+        DbSet<AdventureQuestion> AdventureQuestions { get; }
+        DbSet<UserAdventureQuestionHistory> UserAdventureQuestionsHistory { get; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ LobsterInk.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using LobsterInk.Application.Interfaces;
+using LobsterInk.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LobsterInk.Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Adventure> Adventures { get; }
+        public DbSet<AdventureQuestion> AdventureQuestions { get; }
+        public DbSet<UserAdventureQuestionHistory> UserAdventureQuestionsHistory { get; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
