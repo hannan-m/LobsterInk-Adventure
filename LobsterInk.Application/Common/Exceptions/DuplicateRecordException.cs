9520d277070498ff05a61896b36744f3f6a0ec99
Hannan <hannansultan@outlook.com>
2022-05-09 11:30:00 +0500

    Initial Create custom exceptions and API exception filter
    - DuplicateRecordException
    - NotFoundException
    - ValidationException

---
+++ LobsterInk.Application/Common/Exceptions/DuplicateRecordException.cs
@@ -0,0 +1,9 @@
+﻿namespace LobsterInk.Application.Common.Exceptions;
+
+public class DuplicateRecordException : Exception
+{
+    public DuplicateRecordException(string name, string message)
+        : base($"Entity \"{name}\" with ({message}) already exists.")
+    {
+    }
+}
